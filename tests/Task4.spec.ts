48f4830dd082eac3c41105075210ce7e276bb9ac
crazyministr <crazyministr@gmail.com>
2023-10-14 09:21:10 +0000

    t4 small fix

--- tests/Task4.spec.ts
+++ tests/Task4.spec.ts
@@ -1,5 +1,6 @@
+import '@ton-community/test-utils';
 import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-import { toNano } from 'ton-core';
+import { beginCell, toNano } from 'ton-core';
 import { Task4 } from '../wrappers/Task4';
 
 describe('Task4', () => {
@@ -8,7 +9,7 @@ describe('Task4', () => {
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
-        task4 = blockchain.openContract(await Task4.fromInit());
+        task4 = blockchain.openContract(await Task4.fromInit(1n));
         const deployer = await blockchain.treasury('deployer');
         const deployResult = await task4.send(
             deployer.getSender(),
@@ -29,6 +30,29 @@ describe('Task4', () => {
     });
 
     it('test', async () => {
+        const deployer = await blockchain.treasury('deployer');
+
+        const res1 = await task4.getTime()
+        console.log('time left', res1)
+
+        const res2 = await task4.getNft()
+        console.log('get nft', res2)
+
+        const res3 = await task4.getOwner()
+        console.log('owner', res3)
+        const resCallOwnershipAssigned = await task4.send(  deployer.getSender(),    {
+            value: toNano('0.05'),
+        }, {
+            $$type: 'OwnershipAssigned',
+            queryId: 333n,
+            prevOwner: task4.address,
+            forwardPayload: beginCell().storeUint(13, 32).endCell()
+        })
+        console.log(resCallOwnershipAssigned)
+
+        let t = await task4.getTime()
+        console.log('time left', t)
+
     });
 });
 
